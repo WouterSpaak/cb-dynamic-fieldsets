8d2f969dc971b5e82fa3d0f3f21c3f2e08d7962e
Wouter Spaak <w.spaak@gmail.com>
2018-10-23 10:23:53 +0200

    deletes it all. all of it.

--- src/app/forms-core/base-fieldset.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { ControlContainer } from '@angular/forms';
-
-@Injectable()
-export abstract class BaseFieldset {
-  constructor(readonly parent: ControlContainer) {
-    console.log(parent.control);
-  }
-}
