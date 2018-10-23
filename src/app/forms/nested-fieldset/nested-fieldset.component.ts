3c6990dd8a7dda08e8711df0581648169dffec63
Wouter Spaak <w.spaak@gmail.com>
2018-10-23 11:24:53 +0200

    This shows how to run a side effect from a validator.

--- src/app/forms/nested-fieldset/nested-fieldset.component.ts
+++ src/app/forms/nested-fieldset/nested-fieldset.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormGroup, ValidatorFn } from '@angular/forms';
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { BaseFieldset } from '../fieldset.class';
 
 @Component({
@@ -11,8 +11,14 @@ export class NestedFieldsetComponent extends BaseFieldset {
 
   readonly group = new FormGroup({}, guardedValidator((g: FormGroup) => {
     const [firstFoo, secondFoo] = ['first.foo', 'second.foo'].map(path => g.get(path));
-    return firstFoo.value === secondFoo.value ? null : { nonMatch: true };
+    const error = firstFoo.value === secondFoo.value ? null : { nonMatch: true };
+    runValidatorSideEffect(error, () => secondFoo.setErrors({ fromSideeffect: true }), () => secondFoo.setErrors(null));
+    return error;
   }));
+}
+
+export function runValidatorSideEffect<T, R>(errors: ValidationErrors, effect: (errors: ValidationErrors) => T, restore: () => R) {
+  return errors ? effect(errors) : restore();
 }
 
 export function guardedValidator(validatorFn: ValidatorFn) {
